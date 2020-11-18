f06424f41ba029f70771ee114a3ef32714a94dcb
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-18 19:59:51 +0530

    commit : main.handlebars

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const methodoverride = require("method-override");
 const redis = require("redis");
+const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
+
+//view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//method override
+app.use(methodoverride("_method"));
+
+app.get("/", (req, res, next) => {
+  res.render("searchusers");
+});
 
 app.listen(port, err => {
   console.log(`connect on ${port}`);
