edb16c0d2ad823073228f7dc0167cef9723d3d00
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-19 00:22:09 +0530

    commit : redis connection not connect

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+//create redis client
+let client = redis.createClient();
+
+client.on("connect", function() {
+  console.log("connected to redis");
+});
+
+// client.on("error", function(error) {
+//   console.error(error);
+// });
 //view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -19,6 +29,22 @@ app.use(methodoverride("_method"));
 
 app.get("/", (req, res, next) => {
   res.render("searchusers");
+});
+
+app.post("/user/search", (req, res, next) => {
+  let id = req.body.id;
+  client.hgetall(id, function(err, obj) {
+    if (!obj) {
+      res.render("searchusers", {
+        error: "User does not exist"
+      });
+    } else {
+      obj.id = id;
+      res.render("details", {
+        user: obj
+      });
+    }
+  });
 });
 
 app.listen(port, err => {
